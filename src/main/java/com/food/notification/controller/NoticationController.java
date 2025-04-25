dbd8ce94720fbafeecf5ca800372f9a2118fe623
Selvakumar E <selvakumarelango2003@gmail.com>
2025-04-25 23:08:16 +0530

    Notification service - Initial commit

---
+++ src/main/java/com/food/notification/controller/NoticationController.java
@@ -0,0 +1,9 @@
+package com.food.notification.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notification")
+public class NoticationController {
+}
